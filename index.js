52386db7f8a78e6492d2ed4e441b281ab461652f
Macarealeportelli <macarenarealeportelli@gmail.com>
2021-01-06 21:22:27 -0300

    agregamos ejercicios con arrays de objetos

--- index.js
+++ index.js
@@ -6,10 +6,10 @@
 // const costos = [ 39, 53, 17, 99, 7, 9, 6, 68, 54, 97, 27, 90, 92, 75, 26, 86, 22, 42, 20, 14 ];
 // // codear acá la solución del ejercicio
 
-const costos = [ 39, 53, 17, 99, 7, 9, 6, 68, 54, 97, 27, 90, 92, 75, 26, 86, 22, 42, 20, 14 ];
+const costos = [39, 53, 17, 99, 7, 9, 6, 68, 54, 97, 27, 90, 92, 75, 26, 86, 22, 42, 20, 14];
 
-const losMasCaros = costos.filter((elemento)=>{
-    return elemento>50 
+const losMasCaros = costos.filter((elemento) => {
+    return elemento > 50
 })
 
 console.log(costos);
@@ -25,14 +25,14 @@ console.log(losMasCaros);
 // Utilizando filter crear un nuevo array con todos los números pares y guardalo en la variable numerosPares
 // Utilizando filter crear un nuevo array con todos los números impares y guardalo en la variable numerosImpares
 
-const masNumeros = [ 43, 11, 18, 46, 44, 37, 42, 29, 9, 3, 37, 0, 40, 10, 38, 34, 25, 40, 4, 32 ];
+const masNumeros = [43, 11, 18, 46, 44, 37, 42, 29, 9, 3, 37, 0, 40, 10, 38, 34, 25, 40, 4, 32];
 
-const numerosPares = masNumeros.filter((elemento)=>{
+const numerosPares = masNumeros.filter((elemento) => {
     return elemento % 2 === 0
 });
-const numerosImpares =masNumeros.filter((elemento)=>{
-    return elemento % 2 !=0
-}) ;
+const numerosImpares = masNumeros.filter((elemento) => {
+    return elemento % 2 != 0
+});
 
 
 console.log("pares: ", numerosPares);
@@ -40,3 +40,59 @@ console.log("impares: ", numerosImpares);
 // // deberias tener como resultado
 // // pares: [18, 46, 44, 42, 0, 40, 10, 38, 34, 40, 4, 32]
 // // impares: [43, 11, 37, 29, 9, 3, 37, 25]
+
+
+
+// ARRAYS DE OBJETOS
+
+// Crear un array nuevo con todas las alumnas que tengan menos de 25 años
+// Guardar en una variable a la primera alumna cuyo nombre tenga mas de 4 letras
+// Crear un booleano que diga si todas las alumnas tienen mas de 18 años
+// Crear un booleano que diga si al menos una alumna tiene mas de 30 años
+
+
+const alumnas = [
+    {
+        nombre: 'maca',
+        edad: 22,
+    },
+    {
+        nombre: 'marian',
+        edad: 27,
+    },
+    {
+        nombre: 'rosy',
+        edad: 25,
+    },
+    {
+        nombre: 'caro',
+        edad: 28,
+    },
+    { nombre: 'angie', edad: 38 },
+    { nombre: 'dani', edad: 24 }
+];
+
+
+const menoresDe25 = alumnas.filter((elemento) => {
+    return elemento.edad < 25
+})
+
+console.log(menoresDe25)
+
+const nombreMasDeCuatroLetras = alumnas.find((elemento) => {
+    return elemento.nombre.length > 4
+})
+
+console.log(nombreMasDeCuatroLetras)
+
+const mayoresDe18 = alumnas.every((elemento) => {
+   return elemento.edad > 18 
+})
+
+console.log(mayoresDe18)
+
+const algunaMasDe30 = alumnas.some((elemento)=>{
+    return elemento.edad >30
+})
+
+console.log(algunaMasDe30)
